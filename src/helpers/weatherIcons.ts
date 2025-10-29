8d994aa9711315adca1608167c98a1efe77abe2a
Ana Clara Backer <anabackercontato@gmail.com>
2025-10-29 03:40:10 -0300

    chore: adiciona helper para mapear icons do clima

---
+++ src/helpers/weatherIcons.ts
@@ -0,0 +1,37 @@
+import icon01d from '../assets/weather/01d.png'
+import icon01n from '../assets/weather/01n.png'
+import icon02d from '../assets/weather/02d.png'
+import icon02n from '../assets/weather/02n.png'
+import icon03d from '../assets/weather/03d.png'
+import icon03n from '../assets/weather/03n.png'
+import icon04 from '../assets/weather/04.png'
+import icon09d from '../assets/weather/09d.png'
+import icon09n from '../assets/weather/09n.png'
+import icon10d from '../assets/weather/10d.png'
+import icon10n from '../assets/weather/10n.png'
+import icon11d from '../assets/weather/11d.png'
+import icon11n from '../assets/weather/11n.png'
+import icon13 from '../assets/weather/13.png'
+import icon50d from '../assets/weather/50d.png'
+import icon50n from '../assets/weather/50n.png'
+
+export const weatherIcons: Record<string, string> = {
+  '01d': icon01d,
+  '01n': icon01n,
+  '02d': icon02d,
+  '02n': icon02n,
+  '03d': icon03d,
+  '03n': icon03n,
+  '04d': icon04,
+  '04n': icon04,
+  '09d': icon09d,
+  '09n': icon09n,
+  '10d': icon10d,
+  '10n': icon10n,
+  '11d': icon11d,
+  '11n': icon11n,
+  '13d': icon13,
+  '13n': icon13,
+  '50d': icon50d,
+  '50n': icon50n,
+}
