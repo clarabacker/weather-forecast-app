44711095ee7f94c624d60f77137337aac950c3df
Ana Clara Backer <anabackercontato@gmail.com>
2025-11-12 02:46:58 -0300

    feat(ui): adiciona o componente Alert

---
+++ src/components/UI/Alert/Alert.styles.tsx
@@ -0,0 +1,115 @@
+import styled, { keyframes } from 'styled-components'
+
+interface WrapperProps {
+  $severity: 'success' | 'error' | 'warning' | 'info'
+}
+
+const severityStyles = {
+  success: {
+    primaryColor: '#418944',
+    secondaryColor: '#1E4620',
+    bgColor: '#EDF7ED',
+  },
+  error: {
+    primaryColor: '#D74242',
+    secondaryColor: '#5F2120',
+    bgColor: '#FDEDED',
+  },
+  warning: {
+    primaryColor: '#EF7918',
+    secondaryColor: '#663C00',
+    bgColor: '#FFF4E5',
+  },
+  info: {
+    primaryColor: '#1893D5',
+    secondaryColor: '#014361',
+    bgColor: '#bbdefb',
+  },
+}
+
+const fadeIn = keyframes`
+  0% {
+    top: -1em;
+  }
+  100% {
+    top: 1.2em;
+  }
+`
+
+export const Wrapper = styled.div<WrapperProps>`
+  position: absolute;
+  top: 1.2em;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 0.8em;
+  width: 90%;
+  height: auto;
+  padding: 0.6em 0.8em;
+  border-radius: 6px;
+  background-color: ${({ $severity }) => severityStyles[$severity].bgColor};
+  color: ${({ $severity }) => severityStyles[$severity].primaryColor};
+  border: 2px solid ${({ $severity }) => severityStyles[$severity].primaryColor};
+
+  [data-description],
+  [data-closeicon] {
+    color: ${({ $severity }) => severityStyles[$severity].secondaryColor};
+  }
+
+  animation: ${fadeIn} 0.5s ease-in-out;
+`
+
+export const SeverityIconWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  min-width: 1.4em;
+  background-color: 'transparent';
+  color: inherit;
+
+  svg {
+    width: 100%;
+    height: auto;
+    color: inherit;
+    font-size: 1.4em;
+  }
+`
+
+export const CloseIconButton = styled.button`
+  position: absolute;
+  right: 0.5em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 2px;
+  border: none;
+  outline: none;
+  border-radius: 50%;
+  background-color: transparent;
+  color: inherit;
+  cursor: pointer;
+  overflow: hidden;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+
+  &:active {
+    background-color: rgba(0, 0, 0, 0.2);
+  }
+
+  svg {
+    width: 1.2em;
+    height: 1.2em;
+    color: inherit;
+  }
+
+  animation: background-color 0.2s linear;
+`
+
+export const Description = styled.p`
+  padding-right: 1.3em;
+  font-size: 1em;
+  word-break: break-word;
+`
---
+++ src/components/UI/Alert/index.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import { IoMdClose } from 'react-icons/io'
+import { MdErrorOutline } from 'react-icons/md'
+import { PiWarning } from 'react-icons/pi'
+
+import * as S from './Alert.styles'
+
+interface AlertProps {
+  severity: 'success' | 'error' | 'warning' | 'info'
+  isClosable?: boolean
+  children: React.ReactNode
+}
+
+export const Alert: React.FC<AlertProps> = ({
+  severity,
+  isClosable = false,
+  children,
+}) => {
+  const [visible, setVisible] = useState(true)
+
+  const handleClose = () => setVisible(false)
+
+  if (!visible) return null
+
+  return (
+    <S.Wrapper $severity={severity}>
+      <S.SeverityIconWrapper>
+        {severity === 'error' && <MdErrorOutline></MdErrorOutline>}
+        {severity === 'warning' && <PiWarning></PiWarning>}
+      </S.SeverityIconWrapper>
+
+      <S.Description data-description="true">{children}</S.Description>
+
+      {isClosable && (
+        <S.CloseIconButton data-closeicon="true" onClick={handleClose}>
+          <IoMdClose></IoMdClose>
+        </S.CloseIconButton>
+      )}
+    </S.Wrapper>
+  )
+}
